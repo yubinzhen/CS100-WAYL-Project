a76c4bb8aa582a1bd31935bb61ba55ecf2a9989a
yubinzhen <yubinzhen020105@gmail.com>
2024-10-05 00:51:16 -0700

    Add one more rectangle and compute its area

--- src/main1.cpp
+++ src/main1.cpp
@@ -5,9 +5,15 @@ using namespace std;
 
 int main()
 {
-    Rectangle rect;
-    rect.set_width(3);
-    rect.set_height(4);
-    cout << "Rectangle area: " << rect.area() << endl;
+    Rectangle rect1, rect2;
+    rect1.set_width(3);
+    rect1.set_height(4);
+
+    rect2.set_width(4);
+    rect2.set_height(2);
+
+    cout << "Rectangle 1 area: " << rect1.area() << endl;
+    cout << "Rectangle 2 area: " << rect2.area() << endl;
+
     return 0;
 }
