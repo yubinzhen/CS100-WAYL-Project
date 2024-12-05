4af2ae6934956c502f4498e2b1c5ea850b617eab
ActualCookie88 <kekoa5254@gmail.com>
2024-12-05 09:55:53 -0800

    Start unit tests

---
+++ test/test.cpp
@@ -0,0 +1,43 @@
+#include "../header/battle.h"
+#include "../header/player.h"
+#include "../header/Inventory.h"
+#include "../header/Item.h"
+#include "../gtest/gtest.h"
+/*
+TEST(ItemTests, TestItemConstructor)
+{
+    Item* StandardBall = new Item("Poké Ball", 200, 1, "A Poké Ball is used to catch and store Pokémon.");
+}
+
+TEST(ItemTests, TestGetName)
+{
+    
+    getName();
+}
+TEST(ItemTests, TestGetName)
+{
+    getPrice();
+}
+TEST(ItemTests, TestGetName)
+{
+    addAmount(int n);
+}
+TEST(ItemTests, TestGetName)
+{
+    sellAmount(int n);
+}
+TEST(ItemTests, TestGetName)
+{
+    getAmount();
+}
+TEST(ItemTests, TestGetName)
+{
+    getDescription();
+}
+*/
+
+TEST(PokeballTests, TestPokeballConstructor)
+{
+
+
+}
